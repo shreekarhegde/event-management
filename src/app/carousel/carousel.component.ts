74373329235c53aa87fecf60d68a8873b5335414
Shreekar Ganapati Hegde <shreekarhegde9@gmail.com>
2020-02-02 14:48:38 +0530

    Added carousel and changed top-nav bg-color

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -14,19 +14,20 @@ export class CarouselComponent implements OnInit {
 
   ngOnInit() {
     this.carouselTileItems = [
+      "../../assets/image1.jpeg",
+      "../../assets/image2.jpeg",
       "../../assets/image3.jpeg",
+      "../../assets/image4.jpeg",
       "../../assets/image5.jpeg",
-      "../../assets/image5.jpeg",
-      "../../assets/image5.jpeg",
-      "../../assets/image5.jpeg",
-      "../../assets/image5.jpeg",
-      "../../assets/image5.jpeg",
-      "../../assets/image5.jpeg",
-      "../../assets/image5.jpeg",
-      "../../assets/image5.jpeg",
-      "../../assets/image5.jpeg"
+      "../../assets/imagex.jpeg",
+      "../../assets/WhatsApp Image 2020-01-30 at 10.46.58 PM(2).jpeg",
+      "../../assets/WhatsApp Image 2020-01-30 at 10.46.59 PM(1).jpeg",
+      "../../assets/WhatsApp Image 2020-01-30 at 10.46.59 PM(2).jpeg",
+      "../../assets/WhatsApp Image 2020-01-30 at 10.46.59 PM.jpeg",
+      "../../assets/WhatsApp Image 2020-01-30 at 10.47.00 PM(1).jpeg",
+      "../../assets/WhatsApp Image 2020-01-30 at 10.47.00 PM.jpeg",
+      "../../assets/WhatsApp Image 2020-01-30 at 10.46.59 PM.jpeg"
     ];
-
     this.carouselTile = {
       grid: { xs: 2, sm: 3, md: 3, lg: 5, all: 0 },
       slide: 2,
@@ -42,8 +43,8 @@ export class CarouselComponent implements OnInit {
   public carouselTileLoad(evt: any) {
     const len = this.carouselTileItems.length;
     if (len <= 30) {
-      for (let i = len; i < len; i++) {
-        this.carouselTileItems.push(i);
+      for (let i = len; i < len + 10; i++) {
+        this.carouselTileItems.push("");
       }
     }
   }
